62cf8eeb397da6d7786239551580777d43a9000b
Rajkai Bende <bendr424@gmail.com>
2024-07-31 08:39:21 +0200

    fix: things

--- code/test.py
+++ code/test.py
@@ -1,5 +1,8 @@
 def hello_world():
     print("Hello, world!")
 
+def increment(a: int, b: int) -> int:
+    return a + b
+
 if __name__ == "__main__":
     hello_world()
--- code/test.ts
+++ code/test.ts
@@ -1,10 +1,10 @@
 import * as path from "path";
 
 enum Direction {
-	Up = 1,
-	Down,
-	Left,
-	Right,
+    Up = 1,
+    Down,
+    Left,
+    Right,
 }
 
 let v!;
@@ -17,25 +17,25 @@ const arr: number[] = [1];
 // Comment
 
 type TT = {
-	key: string;
-	num: number;
-	asd: {
-		keys: [];
-	};
+    key: string;
+    num: number;
+    asd: {
+        keys: [];
+    };
 };
 
 const obj = {
-	key: "key",
-	num: 22,
-	asd: {
-		keys: [],
-	},
+    key: "key",
+    num: 22,
+    asd: {
+        keys: [],
+    },
 };
 
 type Optional<T> = T | null | undefined | never;
 type Type<TGeneric> = TGeneric;
 interface Interface {
-	func<T>(a: string, b: T): Type<T>;
+    func<T>(a: string, b: T): Type<T>;
 }
 
 arr[0];
@@ -44,50 +44,56 @@ console.log();
 obj.asd;
 
 class Class implements Interface {
-	public func<T>(a: string, b: T): Type<T> {
-		let acc = 3;
-		let c = () => {
-			({});
-		};
-		const d = null;
-		const obj = {
-			num: num,
-			str: str,
-			a: a,
-			b: acc,
-			c: c,
-			d: d,
-		} as Type<T>;
+    // Comment
+    private readonly vv = 123 + "123";
 
-		const obj2 = {
-			str: "qwe",
-		};
+    constructor() {}
 
-		obj2?.str;
-		obj2["str"];
-		obj2[0];
-		if (a == "2" || true) {
-		}
-		for (let item of arr) {
-		}
+    public func<T>(a: string, b: T): Type<T> {
+        this.vv;
+        let acc = 3;
+        let c = () => {
+            ({});
+        };
+        const d = null;
+        const obj = {
+            num: num,
+            str: str,
+            a: a,
+            b: acc,
+            c: c,
+            d: d,
+        } as Type<T>;
 
-		return obj;
-	}
+        const obj2 = {
+            str: "qwe",
+        };
+
+        obj2?.str;
+        obj2["str"];
+        obj2[0];
+        if (a == "2" || true) {
+        }
+        for (let item of arr) {
+        }
+
+        return obj;
+    }
 }
 
 export function foo(b = "string"): string {
-	let v: TT;
-	const a = new Class();
-	const obj2 = {
-		str: "qwe",
-	};
-	path?.asd.a;
-	obj2[0];
-	return a;
+    let v: TT;
+    const a = new Class();
+    const obj2 = {
+        str: "qwe",
+    };
+    path?.asd.a;
+    obj2[0];
+    return null;
 }
 
 async function f(a: number, b: string, c: boolean) {
-	await f(123, "", false);
+    await f(123, "", false);
 }
 f(123, "Hello", true);
 
@@ -98,7 +104,7 @@ f(123, "Hello", true);
  * @returns
  */
 function add(a: number, b: number): number {
-	return a + b;
+    return a + b;
 }
 add(1, 2);
 
